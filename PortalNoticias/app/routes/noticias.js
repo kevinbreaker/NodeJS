4d6c98a5f3768195d817a90dcf600b5cb17ce6cc
kevinbreaker <kevinbreaker2604@gmail.com>
2017-04-08 17:41:41 -0300

    Portal de notícias

--- PortalNoticias/app/routes/noticias.js
+++ PortalNoticias/app/routes/noticias.js
@@ -1,7 +1,22 @@
 module.exports = function (aplicacao) {
-
+	
 	aplicacao.get('/noticias', function (req,res) {
-		res.render("noticias/noticias");
-
+		var mysql = require('mysql'); // a variavel mysql, vai importar/require, o módulo mysql.
+			//	(que esta dentro do node_modules) para conexão com banco de dados mysql.
+	
+		var connection = mysql.createConnection({ //variavel connection, recebe uma função do modulo mysql( createConnection);
+													// para fazer conexão com o banco, obs: os parametros dentro dele, são em estrutura Json.
+													
+			host : 'localhost', // <- estrutura jason. host é o endereço, no caso do probjeto será local.
+			user : 'root',
+			password : 'kevin',
+			database : 'portal_noticias' 
+																		
+		 	}); //feita a conexão(com info do BD)
+			 
+			connection.query('select * from noticias' ,function (erro, result){    // connection.query(<sql>, <func callback). sql é a consulta em si.
+				res.send(result);	//response.send(envia apenas pra quem requisitou).	// função callback,oque vai ser feito apos a consulta ser realizada. 
+			 																					     	//dentro do node funcionam muito com callback		  						
+			});
 	});
 }
