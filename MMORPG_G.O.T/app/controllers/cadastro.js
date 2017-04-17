f82aaf0f69a5e6db01223d0a86d0b197bd656fd4
kevinbreaker <kevinbreaker2604@gmail.com>
2017-04-17 01:58:29 -0300

    Recebendo e validando cadastro

--- MMORPG_G.O.T/app/controllers/cadastro.js
+++ MMORPG_G.O.T/app/controllers/cadastro.js
@@ -1,4 +1,23 @@
 module.exports.cadastro = (aplicacao,req,res)=>{ 
-	res.render('cadastro')	 // redenriza a view cadastro
+	res.render('cadastro',{validacao : {},dadosForm: {}})	 // redenriza a view cadastro
 
+}
+
+module.exports.cadastrar = (aplicacao,req,res)=>{
+	
+		let dadosForm = req.body
+		
+		req.assert("nome","Preencher o nome, campo Obrigátorio").notEmpty()
+		req.assert("usuario","Preencher o usuário, campo Obrigátorio").notEmpty()
+		req.assert("senha","Preencher a senha, campo Obrigátorio").notEmpty()
+		req.assert("casa","Escolher a casa, campo Obrigátorio").notEmpty()
+		
+		let erro = req.validationErrors()
+		
+		if (erro) {
+			res.render('cadastro',{validacao : erro,dadosForm: dadosForm});
+			return;
+		}
+		res.send("ta tudo correto,relex")
+	
 }
--- MMORPG_G.O.T/app/routes/cadastro.js
+++ MMORPG_G.O.T/app/routes/cadastro.js
@@ -3,4 +3,7 @@ module.exports = (aplicacao)=>{
 		aplicacao.app.controllers.cadastro.cadastro(aplicacao,req,res) // exporta tudo pra o controller cadastro
 	});
 
+	aplicacao.post('/cadastrar',(req,res)=>{
+		aplicacao.app.controllers.cadastro.cadastrar(aplicacao,req,res)	
+	})
 }
