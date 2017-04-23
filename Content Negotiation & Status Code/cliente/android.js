d09d0965e98a3cf86ecee2987edbea6945cfe10f
kevinbreaker <kevinbreaker2604@gmail.com>
2017-04-23 18:25:07 -0300

    server & client.

--- Content Negotiation & Status Code/cliente/android.js
+++ Content Negotiation & Status Code/cliente/android.js
@@ -1,9 +1,19 @@
 let http = require('http');	// importanto http nativo do node.
+
+let opcoes = {
+	hostname: 'localhost',
+	port: 8080,
+	path: '/',
+	headers: {  // 
+		'Accept' : 'application/json' // irá receber um json (por parte do servidor)
+	}										// 'text/html' pra receber em html
+
+}
 	
 let buffer_corpo_response = [];	
 	
 // espera 2 parametros, 1º uma string ou json, 2º uma função de callback que recebe response
-http.get('http://localhost:8080', (res)=>{ // get do http, não do framework express!!!
+http.get(opcoes, (res)=>{ // get do http, não do framework express!!!
 
 	// aparti do on, é possivel identificar alguns estados da req.
 	res.on('data',(pedaco)=>{ // data existe quando req esta em curso, e recebe pedços de info web
--- Content Negotiation & Status Code/servidor/app/routes/index.js
+++ Content Negotiation & Status Code/servidor/app/routes/index.js
@@ -1,5 +1,16 @@
 module.exports = (aplicacao)=>{
 	aplicacao.get('/', (req, res)=>{
-		res.send('Bem vindo a sua app NodeJS!');
+		
+		res.format({ //formatos disponiveis para 'negociar' com a requisição do cliente
+			html: function(){
+				res.send('Bem vindo a sua app NodeJS!');		
+			},
+			json: function(){
+				let retorno = {
+					body: 'Bem vindo a sua app NodeJS'
+				}		
+				res.json(retorno);
+			}
+		});		
 	});
 }
