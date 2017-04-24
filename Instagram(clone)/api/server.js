c56ba775a5329a9e22fe19e0d44c5dc1d08609c5
kevinbreaker <kevinbreaker2604@gmail.com>
2017-04-24 01:13:49 -0300

    Method post, inserindo dados no BD.

--- Instagram(clone)/api/server.js
+++ Instagram(clone)/api/server.js
@@ -5,11 +5,40 @@ let express = require('express'),
 
 let aplicacao = express();
 
-//Configuração body-parser
+//Configuração body-parser - middlewares
 aplicacao.use(bodyParser.urlencoded({extended : true}));
 aplicacao.use(bodyParser.json());
 
+// Escutando na porta 8080 (pq sim!)
 let port = 8080;
 aplicacao.listen(port);
 
+// Estanciando e criando conexão com o bd
+let db = new mongodb.Db(
+	'instagram', // nome do banco
+	new mongodb.Server('localhost',27018, {}),  // obj de conexão do banco
+	{}
+);
+
 console.log("Servidor HTTP esta escutando na porta " + port);
+
+aplicacao.get('/',(req,res)=>{
+	res.send({msg : 'Olá world'});
+})
+
+// POST  (create)
+aplicacao.post('/api',(req,res)=>{
+	let dados = req.body;
+	db.open(function(erro,mongoClient){
+		mongoClient.collection('postagens',(erro,colecao)=>{
+			colecao.insert(dados,(erro,result)=>{
+				if(erro){
+					res.json({'status': 'Erro!'});
+				}else{
+					res.json('status':'Inserido no banco com sucesso');
+				}
+				mongoClient.close();	
+			});		
+		})
+	});
+})
