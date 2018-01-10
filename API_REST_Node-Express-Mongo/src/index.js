332c10792813de1e8ab7b9b4730c5805fb7c8912
Kevin Martin <kevinbreaker2604@gmail.com>
2018-01-10 02:12:36 -0300

    funcionando, em progresso...

--- API_REST_Node-Express-Mongo/src/index.js
+++ API_REST_Node-Express-Mongo/src/index.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./controllers/authController')(app);
-require('./controllers/projectController')(app);
+require('./app/controllers/index')(app);
+//#### fs e path resolvem automaticamente. ###
+// require('./controllers/authController')(app);
+// require('./controllers/projectController')(app);
 
 app.listen(3000);
